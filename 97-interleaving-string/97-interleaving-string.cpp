46b58d4bb001b969971555911511f4b93528a218
Abhinay <adluri123@gmail.com>
2022-02-20 12:07:25 +0530

    Time: 126 ms (6.07%), Space: 6.4 MB (70.78%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void print(vector <vector <bool>> &dp)
+    {
+        for(auto&i:dp)
+        {
+            for(int j=0;j<i.size();j++)
+            {
+                cout<<i[j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    bool isInterleave(string a, string b, string c) 
+    {
+        vector <vector <bool>> dp(a.size()+1,vector <bool> (b.size()+1,false));
+        if(a.size()+b.size()!=c.size())
+        {
+            return false;
+        }
+        dp[0][0]=true;
+        for(int i=0;i<a.size();i++)
+        {
+            if(a[i]==c[i])
+            {
+                dp[i+1][0]=dp[i][0];
+            }
+        }
+        for(int i=0;i<b.size();i++)
+        {
+            if(b[i]==c[i])
+            {
+                dp[0][i+1]=dp[0][i];
+            }
+        }
+        for(int i=0;i<a.size();i++)
+        {
+            for(int j=0;j<b.size();j++)
+            {
+                if(c[i+j+1]==a[i]&&dp[i][j+1])
+                {
+                    dp[i+1][j+1]=true;
+                }
+                else if(c[i+j+1]==b[j]&&dp[i+1][j])
+                {
+                    dp[i+1][j+1]=true;
+                }
+            }
+        }
+        print(dp);
+        return dp[a.size()][b.size()];
+    }
+};
