c1654df4de565bc2c820749f86fa4ec9455f2d29
Abhinay <adluri123@gmail.com>
2022-04-04 16:04:05 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (73.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,55 +1,64 @@
 class Solution {
 public:
-    bool fun(char a,char b)
+    bool cat(char a,char b)
+{
+    if(a=='1'||((a=='2')&&(b<='6')))
     {
-        if((a=='1')||(a=='2'&&b<='6'))
-        {
-            return true;
-        }
-        return false;
+        return true;
     }
-    int numDecodings(string s) 
+    return false;
+}
+    int fun(string a)
+{
+    vector <int> dp(a.size(),0);
+    if(a[0]=='0')
     {
-        if(s[0]=='0')
+        return 0;
+    }
+    dp[0]=1;
+    if(a.size()==1)
+    {
+        return 1;
+    }
+    else if(!cat(a[0],a[1])&&a[1]!='0')
+    {
+        dp[1]=1;
+    }
+    else if(cat(a[0],a[1])&&a[1]!='0')
+    {
+        dp[1]=2;
+    }
+     else if(cat(a[0],a[1])&&a[1]=='0')
+    {
+        dp[1]=1;
+    }   
+    else 
+    {
+        return 0;
+    }
+    for(int i=2;i<a.size();i++)
+    {
+        if(a[i]!='0'&&cat(a[i-1],a[i]))
         {
-            return 0;
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(s.size()==1)
+        else if(a[i]!='0')
         {
-            return 1;
+            dp[i]=dp[i-1];
         }
-        vector <int> dp(s.size(),1);
-        if(fun(s[0],s[1])&&s[1]!='0')
+        else if(cat(a[i-1],a[i]))    
         {
-            dp[1]=2;
-        }
-        else if(fun(s[0],s[1])||s[1]!='0')
-        {
-            dp[1]=1;
+            dp[i]=dp[i-2];
         }
         else
         {
             return 0;
         }
-        for(int i=2;i<s.size();i++)
-        {
-            if(fun(s[i-1],s[i])&&s[i]!='0')
-            {
-                dp[i]=dp[i-1]+dp[i-2];
-            }
-            else if(s[i]!='0')
-            {
-                dp[i]=dp[i-1];
-            }
-            else if(fun(s[i-1],s[i]))
-            {
-                dp[i]=dp[i-2];
-            }
-            else if(s[i]=='0')
-            {
-                dp[i]=0;
-            }
-        }
-        return dp[s.size()-1];
+    }
+    return dp[a.size()-1];
+}
+    int numDecodings(string s) 
+    {
+        return fun(s);
     }
 };
